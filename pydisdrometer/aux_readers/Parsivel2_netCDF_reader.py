fc823f353140a16f3531ccc8b1741d846c45ee93
Eric Sulmoni <Eric.Sulmoni@meteoswiss.ch>
2017-12-12 15:57:25 +0000

    Reflectivity added to Parsivel2 reader

--- pydisdrometer/aux_readers/Parsivel2_netCDF_reader.py
+++ pydisdrometer/aux_readers/Parsivel2_netCDF_reader.py
@@ -58,33 +58,34 @@ class Parsivel2_netCDF(object):
 
         Nd = np.power(10,ma.transpose(ma.array(
                 self.nc_dataset.variables['VolumetricDrops'][:])))
-        velocity = ma.array(
-             [0.05, 0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95, 1.1, 1.3, 1.5, 1.7, 1.9,
-             2.2, 2.6, 3.0, 3.4, 3.8, 4.4, 5.2, 6.0, 6.8, 7.6, 8.8, 10.4, 12.0, 13.6, 15.2,
-             17.6, 20.8])
+        velocity = ma.array([ 0.05,  0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95, 1.1, 1.3 , 1.5 , 1.7 , 1.9 ,
+             		      2.2 ,  2.6 , 3.0 , 3.4 , 3.8 , 4.4 , 5.2 , 6.0 , 6.8 , 7.6 , 8.8, 10.4, 12.0, 13.6, 15.2,
+             		     17.6 , 20.8])
         rain_rate = ma.array(
                 self.nc_dataset.variables['ParsivelIntensity'][:])
-        self.diameter = ma.array(
-			[0.062, 0.187, 0.312, 0.437, 0.562, 0.687, 0.812, 0.937, 1.062, 1.187, 
-			1.375, 1.625, 1.875, 2.125, 2.375, 2.75, 3.25, 3.75, 4.25, 4.75, 
-			5.5, 6.5, 7.5, 8.5, 9.5, 11., 13., 15., 17., 19., 21.5, 24.5])
-        self.spread = ma.array(
-			[0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.250,
-			0.250, 0.250, 0.250, 0.250, 0.500, 0.500, 0.500, 0.500, 0.500, 1.000, 1.000,
-			1.000, 1.000, 1.000, 2.000, 2.000, 2.000, 2.000, 2.000, 3.000, 3.000])
+	Zh = ma.array(
+                self.nc_dataset.variables['Reflectivity'][:])
+
+        diameter = np.array([  0.0625 ,   0.1875 ,   0.3125 ,   0.4375 ,   0.5625 ,   0.6875 ,   0.8125 ,   0.9375 ,   1.0625 ,
+                               1.1875 ,   1.375  ,   1.625  ,   1.875  ,   2.125  ,   2.375  ,   2.75   ,   3.25   ,   3.75   ,
+                               4.25   ,   4.75   ,   5.5    ,   6.5    ,   7.5    ,   8.5    ,    9.5   ,  11.     ,  13.     ,
+                              15.     ,  17.     ,  19.     ,   21.5   ,  24.5    ])
+
+	spread = np.array([ 0.125 ,  0.125 ,  0.125 ,  0.125 ,  0.125 ,  0.125 ,  0.125 ,  0.125 ,  0.125 ,  0.125 ,  0.250 ,
+                            0.250 ,  0.250 ,  0.250 ,  0.250 ,  0.500 ,  0.500 ,  0.500 ,  0.500 ,  0.500 ,  1.000 ,  1.000 ,
+                            1.000 ,  1.000 ,  1.000 ,  2.000 ,  2.000 ,  2.000 ,  2.000 ,  2.000 ,  3.000 ,  3.000 ])
 
         # TODO: Move this to new metadata utility, and just add information from raw netcdf where appropriate
         self.bin_edges = common.var_to_dict(
                 'bin_edges',
-                np.hstack((0, self.diameter + np.array(self.spread) / 2)),
+                np.hstack((0, diameter + np.array(spread) / 2)),
                 'mm', 'Boundaries of bin sizes')
         self.spread = common.var_to_dict(
-                'spread', self.spread,
+                'spread', spread,
                 'mm', 'Bin size spread of bins')
         self.diameter = common.var_to_dict(
-                'diameter', self.diameter,
+                'diameter', diameter,
                 'mm', 'Particle diameter of bins')
-
         self.fields['Nd'] = common.var_to_dict(
                 'Nd', np.ma.masked_where(Nd<1,Nd), 'm^-3 mm^-1',
                 'Liquid water particle concentration')
@@ -94,6 +95,10 @@ class Parsivel2_netCDF(object):
         self.fields['rain_rate'] = common.var_to_dict(
                 'rain_rate', rain_rate, 'mm h^-1',
                 'Rain rate')
+	self.fields['Zh'] = common.var_to_dict(
+                'Zh', Zh, 'dBZ',
+                'Reflectivity')
+
 
         # self.fields['num_drop'] = common.var_to_dict(
                # "num_drop", self.nc_dataset.variables['num_drop'][:], '#',
