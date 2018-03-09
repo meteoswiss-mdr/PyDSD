318bcaebbb001376d5f2281cd9348af089f543f0
Eric Sulmoni <Eric.Sulmoni@meteoswiss.ch>
2018-03-09 08:34:35 +0000

    pycodestyle Parsivel2_netCDF_reader.py

--- pydsd/aux_readers/Parsivel2_netCDF_reader.py
+++ pydsd/aux_readers/Parsivel2_netCDF_reader.py
@@ -24,7 +24,7 @@ def read_parsivel2_netCDF(filename):
     DropSizeDistrometer object
 
     '''
-
+    
     reader = Parsivel2_netCDF(filename)
 
     if reader:
@@ -37,7 +37,7 @@ def read_parsivel2_netCDF(filename):
 
 class Parsivel2_netCDF(object):
     '''
-    This class reads and parses parsivel2 disdrometer data from ARM netcdf
+    This class reads and parses parsivel2 disdrometer data from OTT netcdf
     files. These conform to document (Need Document).
 
     Use the read_parsivel2_netCDF() function to interface with this.
@@ -59,47 +59,46 @@ class Parsivel2_netCDF(object):
         del self.time['_FillValue']
 
         self.fields['Nd'] = common.ncvar_to_dict(self.nc_dataset.variables['VolumetricDrops'])
-        self.fields['Nd']['data'] = ma.transpose(np.power(10,self.fields['Nd']['data']))
-        ma.set_fill_value(self.fields['Nd']['data'],0.)
+        self.fields['Nd']['data'] = ma.transpose(np.power(10, self.fields['Nd']['data']))
+        ma.set_fill_value(self.fields['Nd']['data'], 0.)
         self.fields['Nd']['data'] = self.fields['Nd']['data'].filled()
         del self.fields['Nd']['_FillValue']
         self.fields['Nd']['units'] = "1/m^3 1/mm"
-        #ma.masked_less(self.fields['Nd']['data'], 1.0)
-        #ma.set_fill_value(self.fields['Nd']['data'],np.power(10,self.fields['Nd']['data'].fill_value))
+        # ma.masked_less(self.fields['Nd']['data'], 1.0)
+        # ma.set_fill_value(self.fields['Nd']['data'], np.power(10, self.fields['Nd']['data'].fill_value))
 
-        self.fields['RR']  = common.ncvar_to_dict(self.nc_dataset.variables['ParsivelIntensity'])
+        self.fields['RR'] = common.ncvar_to_dict(self.nc_dataset.variables['ParsivelIntensity'])
         self.fields['RR']['data'] = ma.masked_array(self.fields['RR']['data'])
-        ma.set_fill_value(self.fields['RR']['data'],self.fields['RR']['_FillValue'])
+        ma.set_fill_value(self.fields['RR']['data'], self.fields['RR']['_FillValue'])
         del self.fields['RR']['_FillValue']
 
-        self.fields['Zh']  = common.ncvar_to_dict(self.nc_dataset.variables['Reflectivity'])
+        self.fields['Zh'] = common.ncvar_to_dict(self.nc_dataset.variables['Reflectivity'])
         del self.fields['Zh']['_FillValue']
 
-        self.fields['num_particles']  = common.ncvar_to_dict(self.nc_dataset.variables['RawDrops'])
+        self.fields['num_particles'] = common.ncvar_to_dict(self.nc_dataset.variables['RawDrops'])
         self.fields['num_particles']['data'] = ma.masked_array(self.fields['num_particles']['data'])
-        ma.set_fill_value(self.fields['num_particles']['data'],self.fields['num_particles']['_FillValue'])
+        ma.set_fill_value(self.fields['num_particles']['data'], self.fields['num_particles']['_FillValue'])
         del self.fields['num_particles']['_FillValue']
-        
-        self.fields['terminal_velocity']  = common.ncvar_to_dict(self.nc_dataset.variables['VelocityDrops'])
+
+        self.fields['terminal_velocity'] = common.ncvar_to_dict(self.nc_dataset.variables['VelocityDrops'])
         self.fields['terminal_velocity']['data'] = ma.transpose(ma.masked_array(self.fields['terminal_velocity']['data']))
-        ma.set_fill_value(self.fields['terminal_velocity']['data'],self.fields['terminal_velocity']['_FillValue'])
+        ma.set_fill_value(self.fields['terminal_velocity']['data'], self.fields['terminal_velocity']['_FillValue'])
         del self.fields['terminal_velocity']['_FillValue']
 
-        self.fields['Precip_Code']  = common.ncvar_to_dict(self.nc_dataset.variables['PrecipCode'])
+        self.fields['Precip_Code'] = common.ncvar_to_dict(self.nc_dataset.variables['PrecipCode'])
 
-        
-        diameter = ma.array([  0.0625 ,   0.1875 ,   0.3125 ,   0.4375 ,   0.5625 ,   0.6875 ,   0.8125 ,   0.9375 ,   1.0625 ,
-                               1.1875 ,   1.375  ,   1.625  ,   1.875  ,   2.125  ,   2.375  ,   2.75   ,   3.25   ,   3.75   ,
-                               4.25   ,   4.75   ,   5.5    ,   6.5    ,   7.5    ,   8.5    ,    9.5   ,  11.     ,  13.     ,
-                              15.     ,  17.     ,  19.     ,   21.5   ,  24.5    ])
+        diameter = ma.array([0.0625, 0.1875, 0.3125, 0.4375, 0.5625, 0.6875, 0.8125, 0.9375, 1.0625,
+                            1.1875, 1.375, 1.625, 1.875, 2.125, 2.375, 2.75, 3.25, 3.75,
+                            4.25, 4.75, 5.5, 6.5, 7.5, 8.5, 9.5, 11., 13.,
+                            15., 17., 19., 21.5, 24.5])
 
-        spread = ma.array([ 0.125 ,  0.125 ,  0.125 ,  0.125 ,  0.125 ,  0.125 ,  0.125 ,  0.125 ,  0.125 ,  0.125 ,  0.250 ,
-                            0.250 ,  0.250 ,  0.250 ,  0.250 ,  0.500 ,  0.500 ,  0.500 ,  0.500 ,  0.500 ,  1.000 ,  1.000 ,
-                            1.000 ,  1.000 ,  1.000 ,  2.000 ,  2.000 ,  2.000 ,  2.000 ,  2.000 ,  3.000 ,  3.000 ])
-        
-        velocity = ma.array([ 0.05,  0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95, 1.1, 1.3 , 1.5 , 1.7 , 1.9 ,
-             		      2.2 ,  2.6 , 3.0 , 3.4 , 3.8 , 4.4 , 5.2 , 6.0 , 6.8 , 7.6 , 8.8, 10.4, 12.0, 13.6, 15.2,
-             		     17.6 , 20.8 ])
+        spread = ma.array([0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.250,
+                          0.250, 0.250, 0.250, 0.250, 0.500, 0.500, 0.500, 0.500, 0.500, 1.000, 1.000,
+                          1.000, 1.000, 1.000, 2.000, 2.000, 2.000, 2.000, 2.000, 3.000, 3.000])
+
+        velocity = ma.array([0.05, 0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95, 1.1, 1.3, 1.5, 1.7, 1.9,
+                            2.2, 2.6, 3.0, 3.4, 3.8, 4.4, 5.2, 6.0, 6.8, 7.6, 8.8, 10.4, 12.0, 13.6, 15.2,
+                            17.6, 20.8])
 
         self.bin_edges = common.var_to_dict(
                 'bin_edges',
@@ -111,12 +110,12 @@ class Parsivel2_netCDF(object):
         self.diameter = common.var_to_dict(
                 'diameter', diameter,
                 'mm', 'Particle diameter of bins')
-        #self.fields['terminal_velocity'] = common.var_to_dict(
+        # self.fields['terminal_velocity'] = common.var_to_dict(
         #        'velocity', velocity, 'm s^-1',
         #        'Terminal fall velocity for each bin')
-        
+
         for key in self.nc_dataset.ncattrs():
-            self.info[key] =self.nc_dataset.getncattr(key)
+            self.info[key] = self.nc_dataset.getncattr(key)
 
     def get_datetime_from_epoch(self, sample_times):
         """Convert time from epoch time to datetime and return a dictionary."""
